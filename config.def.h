6de8126a2d99bda00a6472bb762404a4651c3428
KC5-BP <kevin.bougnonpeigne@etml-es.ch>
2024-03-22 02:08:15 +0100

    Keep only 4 tags/workspaces + Add dedicated tag to apps

--- config.def.h
+++ config.def.h
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tags[] = { "", "", "", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { ">_", "www", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -30,8 +30,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	//{ "Gimp",         NULL,       NULL,            0,       1,           -1 },
+	{ "qutebrowser",  NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "steam",        NULL,       NULL,       1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
