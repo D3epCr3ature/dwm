c8439229f5a50053f3c1df54bca7a44eac71d6d4
D3epCr3ature <kcs.bp@ikmail.com>
2025-07-26 00:28:37 +0200

    Sound management (Mute toggle, [In|De]crease)

--- config.def.h
+++ config.def.h
@@ -1,4 +1,6 @@
 /* See LICENSE file for copyright and license details. */
+/* https://cgit.freedesktop.org/xorg/proto/x11proto/tree/XF86keysym.h */
+#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
@@ -63,6 +65,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
+/* '-> sound */
+#define SOUND_DEVICE "@DEFAULT_SINK@"
+static const char *volumeToggle[] = { "pactl", "set-sink-mute", SOUND_DEVICE, "toggle", NULL };
+static const char *volumeIncr[] = { "pactl", "set-sink-volume", SOUND_DEVICE, "+5%", NULL };
+static const char *volumeDecr[] = { "pactl", "set-sink-volume", SOUND_DEVICE, "-5%", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -71,6 +78,10 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
         /* custom */
+        /* '-> sound */
+        { 0,                            XF86XK_AudioMute, spawn,   {.v = volumeToggle } },
+        { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeIncr } },
+        { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumeDecr } },
         /* behavior-like */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
